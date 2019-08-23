5ec2789cc65142c7c9433fcb56e0995d40f72ade
zl19971127 <2216516246@qq.com>
2019-08-23 14:50:10 +0800

    first commit

---
+++ 05-数据类型.py
@@ -0,0 +1,43 @@
+# 整型： 整数 int
+num = 10
+
+# print(num1)
+print(type(num))
+
+# 浮点型: 小数  float
+num_1 = 30.0
+print(type(num_1))
+
+# 字符串 str -- 但凡带引号的数据都是字符串数据；所有字符串数据都要有引号
+str1 = 'daqiu'
+print(type(str1))
+
+
+# 认识的即可
+# 布尔型 bool： 只有两个值 True 和 False(boolean); 判断真假
+a = True
+print(type(a))
+
+# 列表: list, 可以一次性存储多个数据
+b = [10, 20, 30]
+print(type(b))
+
+# 字典dict    xx 对应 xx 键key对应值value. k对应v k:v 姓名是xx 年龄是xx 手机号是xxx
+# 字典 1. k：v(键值对）；2. 一个字典可以放多个键值对，各个键值对用逗号隔开
+c = {"name": "daqiu", "age": 18}
+print(type(c))
+
+# 元组 tuple; 存储不能修改的数据
+d = (10, 20, 30)
+print(type(d))
+
+# 集合 set 数据不允许重复
+e = {10, 20, 30, 20}
+print(type(e))
+print(e)
+
+
+
+
+
+
